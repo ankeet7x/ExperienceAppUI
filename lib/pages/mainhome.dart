91ab43ed6e4b9cce3a70549dcd07dfe198b30c13
ankeet7x <ankeetkarki30@gmail.com>
2020-08-08 18:24:02 +0545

    Latest

--- lib/pages/mainhome.dart
+++ lib/pages/mainhome.dart
@@ -107,6 +107,7 @@ class _HomeState extends State<Home> {
             ),
             sizedBoxGap(),
             buildTypeRow(),
+            sizedBoxGap(),
             sizedBoxGap()
           ],
         ),
